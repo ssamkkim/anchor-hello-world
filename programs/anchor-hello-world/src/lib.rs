5fd0d4079c58d59b6a193f270184d20d895fdfea
ssamkkim <ssamuel.kkim@gmail.com>
2023-05-08 23:19:53 -0700

    up to date fixes

--- programs/anchor-hello-world/src/lib.rs
+++ programs/anchor-hello-world/src/lib.rs
@@ -6,17 +6,17 @@ declare_id!("7LULzYja5GZKq7Kcy6TgVCJYpHgFVyQmQ4c1FwZqVNMj");
 pub mod anchor_hello_world {
     use super::*;
 
-    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
 
         if topic.chars().count() > 50 {
-            return Err(ErrorCode::TopicTooLong.into())
+            return Err(error!(ErrorCode::TopicTooLong))
         }
 
         if content.chars().count() > 280 {
-            return Err(ErrorCode::ContentTooLong.into())
+            return Err(error!(ErrorCode::ContentTooLong))
         }
 
         tweet.author = *author.key;
